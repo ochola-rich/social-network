51960cd37e5ce70dafdf338bcd310c3d103ffc3d
riotieno <richardochola3@gmail.com>
2026-06-14 11:30:03 +0300

    Task : adde the interface contract to communicate with the database

--- backend/internal/follows/repository.go
+++ backend/internal/follows/repository.go
@@ -1,1 +1,12 @@
 package follows
+
+import "context"
+
+type Repository interface {
+	GetUser(ctx context.Context, userID int) (*User, error)
+	GetRelationshipByUsers(ctx context.Context, followerID, followingID int) (*Relationship, error)
+	GetRelationshipByID(ctx context.Context, relationshipID int) (*Relationship, error)
+	CreateRelationship(ctx context.Context, followerID, followingID int, status FollowStatus) (*Relationship, error)
+	UpdateRelationshipStatus(ctx context.Context, relationshipID int, status FollowStatus) (*Relationship, error)
+	DeleteRelationship(ctx context.Context, relationshipID int) error
+}
